7efac4b3ae31d456bd772cbe08c1de88b3a0ebfd
Malin <malin.radtke@uni-oldenburg.de>
2024-05-15 14:14:28 +0200

    Modify complex use cases for analysis

--- agent_communication_generation_tool/complex_use_cases/complex_ev_management.py
+++ agent_communication_generation_tool/complex_use_cases/complex_ev_management.py
@@ -8,7 +8,8 @@ sys.path.append(Path(__file__).parent.parent.parent.absolute().__str__())
 from network_generation.simbench_network_extractor import SystemState
 from agent_communication_generation_tool.complex_use_cases.simulation_run_variables import \
     complexities, overlay_types, data_size_generators
-from agent_communication_generation_tool.description_classes.simbench_codes import simbench_codes_low_voltage
+from agent_communication_generation_tool.description_classes.simbench_codes import simbench_codes_low_voltage, \
+    simbench_codes_analysis
 from agent_communication_generation_tool.description_classes.communication_scenario_description import \
     CommunicationScenarioDescription
 from agent_communication_generation_tool.description_classes.agent_communication_pattern import \
@@ -21,9 +22,9 @@ SIMULATION_DURATION_MS = 30 * 1000  # 30 seconds
 
 num_iterations_till_goal = [1, 5]
 
-for max_number_of_agents_ in [random.randint(2, 100) for _ in range(5)]:
+for max_number_of_agents_ in [random.randint(2, 100) for _ in range(2)]:
     for system_state in [SystemState.NORMAL, SystemState.LIMITED, SystemState.FAILED]:
-        simbench_codes = random.sample(simbench_codes_low_voltage, 2)
+        simbench_codes = simbench_codes_analysis
         for simbench_code in simbench_codes:
             for network_description_class in [Simbench5GNetworkDescription, SimbenchLTENetworkDescription]:
                 if network_description_class == SimbenchLTENetworkDescription:
